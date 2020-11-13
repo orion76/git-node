242c6bdec1c3676250abd5800175314c907c4af1
Paul <gpb@yandex.ru>
2020-11-13 06:19:53 +0400

    init

--- bin/src/hooks/pre-commit.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pre_commit_command_1 = require("../commands/pre-commit.command");
-pre_commit_command_1.pre_commit();
-//# sourceMappingURL=pre-commit.js.map
